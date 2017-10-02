883c569784e33e047e2fac4bcf4df8010d50e0fe
Brian Parrish <brian.parrish3739@gmail.com>
2017-10-02 15:13:06 -0400

    added health effects and attack button, but raw currently

--- src/sprites/Infantry.js
+++ src/sprites/Infantry.js
@@ -1,12 +1,14 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
-  constructor ({ game, x, y, asset, width, height }) {
+  constructor ({ game, x, y, asset, width, height, health, AP }) {
     super(game, x, y, asset)
     this.anchor.setTo(0.5)
     this.scale.setTo(0.9)
     this.width = width
     this.height = height
+    this.health = health
+    this.AP = AP;
   }
 
   update () {
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,6 +1,6 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Infantry from '../sprites/Infantry'
 import Block from '../sprites/Block'
 // import EasyStar from 'easystarjs'
 // var easystar = new EasyStar.js();
@@ -14,34 +14,45 @@ export default class extends Phaser.State {
 
   create () {
     this.blocks = this.add.group()
-    this.mushroom1 = new Mushroom({
-      game: this.game,
-      x: 775,
-      y: 25,
-      asset: 'mushroom',
-      width: 23,
-      height: 23
-    })
-    this.mushroom2 = new Mushroom({
-      game: this.game,
-      x: 25,
-      y: 775,
-      asset: 'mushroom',
-      width: 23,
-      height: 23
-    })
-    this.mush1 = this.game.add.existing(this.mushroom1)
+    this.pieces = {
+      1: this.infantry1 = new Infantry({
+        game: this.game,
+        x: 775,
+        y: 25,
+        asset: 'mushroom',
+        width: 23,
+        height: 23,
+        health: 10, 
+        AP: 10
+      }),
+      2: this.infantry2 = new Infantry({
+        game: this.game,
+        x: 25,
+        y: 775,
+        asset: 'mushroom',
+        width: 23,
+        height: 23,
+        health: 10,
+        AP: 10
+      })
+    };
+    console.log(this.pieces);
+
+    this.mush1 = this.game.add.existing(this.infantry1)
+    console.log('mush', this.infantry1);
     this.mush1.player = 1
-    this.mush2 = this.game.add.existing(this.mushroom2)
+    this.mush2 = this.game.add.existing(this.infantry2)
     this.mush2.player = 2
     this.mush1.inputEnabled = true
     this.mush1.events.onInputDown.add(this.showMoves, this)
     this.mush2.events.onInputDown.add(this.showMoves, this)
+
     for (var i = 25; i < 800; i = i + 25) {
       for (var j = 25; j < 800; j = j + 25) {
         this.createBlock(i, j, 'greenSquare')
       }
     }
+
     var style = { font: '20px Arial', fill: '#fff' }
     this.game.add.text(410, 20, 'Player:', style)
     this.game.add.text(540, 20, 'Fun:', style)
@@ -49,12 +60,14 @@ export default class extends Phaser.State {
     this.playerText = this.game.add.text(480, 20, this.currentPlayer, style)
     this.funText = this.game.add.text(585, 20, '11', style)
   }
+
   togglePlayer () {
     this.currentPlayer = this.currentPlayer === 1 ? 2 : 1
     this.mush1.inputEnabled = this.currentPlayer === 1
     this.mush2.inputEnabled = this.currentPlayer === 2
     this.playerText.text = this.currentPlayer
   }
+
   showMoves (sprite, event) {
     if (sprite.player === this.currentPlayer) {
       var alpha = this.showingBlue ? 1 : 0.5
@@ -80,24 +93,40 @@ export default class extends Phaser.State {
 
   moveHere (sprite, event) {
     if (this.showingBlue) {
-      this.mushroomMovement = this.currentPlayer === 1 ? this.game.add.tween(this.mush1) : this.game.add.tween(this.mush2)
+      this.changePosition = this.currentPlayer === 1 ? this.game.add.tween(this.mush1) : this.game.add.tween(this.mush2)
       this.blocks.children.forEach((ele) => {
         ele.alpha = 1
         ele.inputEnabled = false
       }, this)
-      this.mushroomMovement.to({x: sprite.x, y: sprite.y}, 350)
-      this.mushroomMovement.start()
-      this.mushroomMovement.onComplete.add(function () {
-        this.mushroomMovement.timeline = []
+      this.changePosition.to({x: sprite.x, y: sprite.y}, 350)
+      this.changePosition.start()
+      this.changePosition.onComplete.add(function () {
+        this.changePosition.timeline = []
       }, this)
+      this.button = this.game.add.button(this.game.world.centerX, this.game.world.centerY, 'mushroom', this.attackPiece, this, 2, 1, 0);
     }
     this.showingBlue = false
+  }
+
+  attackPiece() {
+    this.button.pendingDestroy = true;
+    let playerToAttack = this.currentPlayer === 1 ? 2 : 1;
+    let attackingPlayer = this.currentPlayer === 1 ? 1 : 2;
+    let pieceToAttack = playerToAttack === 1 ? this.mush1 : this.mush2;
+    let attackingPiece = attackingPlayer === 1 ? this.mush2 : this.mush1;
+    pieceToAttack.health -= attackingPiece.AP;
     this.togglePlayer()
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom1, 32, 32)
+      this.game.debug.spriteInfo(this.infantry1, 32, 32)
     }
+  }
+
+  update() {
+    //check current health
+    this.mush1.health <= 0 ? this.mush1 = undefined : null;
+    this.mush2.health <= 0 ? this.mush2 = undefined : null;
   }
 }
