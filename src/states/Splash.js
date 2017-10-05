4860adc2796d66668aacefb34bda49936b1c0c6c
Brian Parrish <brian.parrish3739@gmail.com>
2017-10-05 10:34:50 -0400

    real changes

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -14,6 +14,7 @@ export default class extends Phaser.State {
     // load your assets
     //
     this.load.image('infantry', 'assets/images/infantry.png')
+    this.load.image('infantry_blue', 'assets/images/infantry_blue.png')
     this.load.image('waitSprite', 'assets/images/wait.png')
     this.load.image('fireSprite', 'assets/images/fireSprite.png')
     this.load.image('target', 'assets/images/defenderTarget.png');
